b7f57f10e819f08848a79e1a1d8852c5565fc646
Emir Raimberdiev <emir.raimberdiev@gmail.com>
2024-11-28 21:34:28 +0600

    Add files via upload

---
+++ site/images/index.js
@@ -0,0 +1,7 @@
+// Optional: Add JavaScript if you want additional interactivity
+document.querySelectorAll('.photo').forEach(photo => {
+    photo.addEventListener('click', () => {
+      alert('You clicked on a photo!');
+    });
+  });
+  
